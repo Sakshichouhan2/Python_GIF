24b58f7c920aefa2d5a8d2372565fcc0f78f5ac2
chouhansakshi88@gmail.com <chouhansakshi88@gmail.com>
2020-11-24 11:47:30 +0530

    Python Script to Create GIF

--- gif.py
+++ gif.py
@@ -1,16 +1,17 @@
-
 from PIL import Image
 import glob
 
 # Create the frames
 frames = []
-imgs = glob.glob("*.png")
+imgs = list(glob.glob("*.png"))
 for i in imgs:
     new_frame = Image.open(i)
     frames.append(new_frame)
+    #print(new_frame)
+    print(imgs)
 
 # Save into a GIF file that loops forever
-frames[0].save('png_to_gif.gif', format='GIF',
-               append_images=frames[1:2],
+frames[1].save('png_to_gif.gif', format='GIF',
+               append_images=frames[0:1],
                save_all=True,
                duration=300, loop=0)
